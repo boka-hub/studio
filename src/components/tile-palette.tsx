e9ec9f8c66f21ff87a9763e3436fe043712be494
Bonmanush <riyalbonmanush@gmail.com>
2025-08-24 06:06:36 +0000

    didn't we agree on three col expanded palette.... what is ts?😊

--- src/components/tile-palette.tsx
+++ src/components/tile-palette.tsx
@@ -324,7 +324,7 @@ export const TilePalette: FC<TilePaletteProps> = ({
       <ScrollArea className="flex-grow">
         <div className={cn(
             "grid gap-2 p-4 pt-2",
-            isCollapsed ? 'grid-cols-1 justify-items-center' : 'grid-cols-2'
+            isCollapsed ? 'grid-cols-1 justify-items-center' : 'grid-cols-3'
           )}>
           {filteredTiles.map((tile) => (
               <div 
